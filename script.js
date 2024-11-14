5d7c44a200605bb4bfd70ae3b0feafd2e7c36ee4
killian fievet <killianfievet@gmail.com>
2024-11-14 12:53:29 +0100

    Update

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var buttonclicked = document.getElementById("button-generate");
 var Resultat = document.getElementById("returnResultat");
-var GetValue = document.getElementById("longueurPwd").value;
+var GetValue = document.getElementById("longueurPwd");
+var Erreur = document.getElementById("returnErreur");
 
 // tableau
 const tableau = [
@@ -19,22 +20,36 @@ const tableau = [
 ];
 
 function buttonClick() {
-  if (GetValue == "") {
-    alert("Veuillez entrer une longueur");
+  document.addEventListener("click", Click);
+
+  var Longueur = GetValue.value;
+  if (Longueur == "") {
+    return MessageErreur();
   } else {
-    var password = "";
-    var length = parseInt(GetValue);
-    for (var i = 0; i < length; i++) {
-      var random = Math.floor(Math.random() * 4);
-      var random2 = Math.floor(
-        Math.random() * tableau[random].Majuscule.length
-      );
-      password += tableau[random].Majuscule[random2];
-    }
+    alert(Longueur);
   }
-  password.innerHTML = `<p>${password}`;
 }
 
+function Click() {
+  buttonclicked.style.transform = "scale(1.1)";
+  buttonclicked.style.transition = "0.5s";
+}
+
+function MessageErreur() {
+  Erreur.innerHTML = `<p>Veuillez entrer une longueur</p>`;
+}
 // alert("OUI");
 // var password = generatePassword(GetValue);
 // Resultat.innerHTML = `<p>${password}`;
+
+// var password = "";
+//     var length = parseInt(GetValue);
+//     for (var i = 0; i < length; i++) {
+//       var random = Math.floor(Math.random() * 4);
+//       var random2 = Math.floor(
+//         Math.random() * tableau[random].Majuscule.length
+//       );
+//       password += tableau[random].Majuscule[random2];
+//     }
+//   }
+//   password.innerHTML = `<p>${password}`;
